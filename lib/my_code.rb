032b23976d37c740ccbefb9716926f6b5c19c337
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-10 07:11:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,8 +10,9 @@ end
 
 def reduce (data, starting_point = 0)
   i = 0
-  new_data = nil
-  # new_data += starting_point
+  if starting_point
+    new_data = starting_point
+  end
   while i < data.length do
     new_data = yield(new_data, data[i])
     i += 1
