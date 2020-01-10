667abab47891e69ee4f0dea2c6621a16c57db681
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-10 06:38:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,9 @@
 def map (data)
-  # i = 0
-  # new_data = []
-  # while i < data.length do
-    yield(data)
-    # i += 1
-  # end
-  # new_data
+  i = 0
+  new_data = []
+  while i < data.length do
+    new_data << yield(data)
+    i += 1
+  end
+  new_data
 end
