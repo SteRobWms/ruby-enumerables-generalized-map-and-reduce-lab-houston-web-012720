47add9f360548a9e36e9ac6c470be7375241cbec
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-10 07:45:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,9 +12,11 @@ def reduce (data, starting_point = nil)
   if starting_point
     new_data = starting_point
     i = 0
+# I missed the else section from here...
   else
     new_data = data[0]
     i = 1
+# ...to here. And the "if" conditional, of course.
   end
   while i < data.length do
     new_data = yield(new_data, data[i])
